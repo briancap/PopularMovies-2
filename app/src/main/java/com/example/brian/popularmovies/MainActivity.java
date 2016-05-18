bc3fbb7ad5ffc2d718f29addde1a40522059a9c2
Brian <btc5081@gmail.com>
2016-05-17 19:00:37 -0500

    add menu methods to MainActivity

--- app/src/main/java/com/example/brian/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/brian/popularmovies/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.brian.popularmovies;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     static final String LOG_TAG = "**********   Movies Fragment    **********";
@@ -17,6 +19,23 @@ public class MainActivity extends AppCompatActivity {
                             .add(R.id.container, new MoviesFragment())
                             .commit();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if (id == R.id.sort_order){
+            //TODO: launch intent to go to SettingsActivity
+        }
+
+        return true;
     }
 
 
