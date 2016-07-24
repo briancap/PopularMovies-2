39b7967b21bea3ad973186a174c6f28a3705329c
Brian <btc5081@gmail.com>
2016-07-23 23:39:16 -0500

    Add layout to Movie Detail Frag

--- app/src/main/java/com/example/brian/popularmovies/MovieDetailActivity.java
+++ app/src/main/java/com/example/brian/popularmovies/MovieDetailActivity.java
@@ -1,9 +1,14 @@
 package com.example.brian.popularmovies;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by brian on 5/17/16.
@@ -11,21 +16,37 @@ import android.view.Menu;
  * Display Movie title, image, plot summary user rating, and release date to details activity
  */
 public class MovieDetailActivity extends AppCompatActivity {
-
-    public void MovieDetailActivity(){
-
-    }
+    static final String LOG_TAG = "*MoviesDetailActivity*";
+    HashMap<String, Object> oneMovieData;
+    final String TITLE_TAG = "original_title";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies_detail);
+
+        Intent intent = getIntent();
+        if(intent != null) {
+            //String test = (String) intent.getExtras().get("map");
+            oneMovieData = (HashMap) intent.getSerializableExtra("map");
+            //Log.e(LOG_TAG, oneMovieData.get(TITLE_TAG).toString());
+        } else {
+            Log.e(LOG_TAG, "movie data extra from intent is null");
+        }
+
         if(savedInstanceState == null){
+            Bundle bundle = new Bundle();
+            bundle.putSerializable("map", oneMovieData);
+
+            MoviesDetailFragment moviesDetailFragment = new MoviesDetailFragment();
+            moviesDetailFragment.setArguments(bundle);
+
             FragmentManager fragmentManager = getSupportFragmentManager();
             fragmentManager.beginTransaction()
-                    .add(R.id.container_detail, new MoviesDetailFragment())
+                    .add(R.id.container_detail, moviesDetailFragment)
                     .commit();
         }
+
     }
 
     @Override
--- app/src/main/java/com/example/brian/popularmovies/MoviesDetailFragment.java
+++ app/src/main/java/com/example/brian/popularmovies/MoviesDetailFragment.java
@@ -1,13 +1,14 @@
 package com.example.brian.popularmovies;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.Map;
 
@@ -15,16 +16,48 @@ import java.util.Map;
  * Created by brian on 7/23/16.
  */
 public class MoviesDetailFragment extends Fragment {
+
+    Map<String, Object> oneMovieData;
+
+    final String TITLE_TAG = "original_title";
+    final String THUMBNAIL_TAG = "poster_path";
+    final String OVERVIEW_TAG = "overview";
+    final String RATING_TAG = "vote_average";
+    final String RELEASE_DATE_TAG = "release_date";
+
+    public MoviesDetailFragment(){
+
+    }
+
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(false);
 
+        Bundle bundle = getArguments();
+        oneMovieData = (Map) bundle.get("map");
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container
             , Bundle savedInstanceState){
 
         View rootView = inflater.inflate(R.layout.movies_detail_fragment, container, false);
+
+        TextView    title       = (TextView) rootView.findViewById(R.id.Title);
+        ImageView   thumbnail   = (ImageView) rootView.findViewById(R.id.Thumbnail);
+        TextView    releaseDate = (TextView) rootView.findViewById(R.id.ReleaseDate);
+        TextView    runtime     = (TextView) rootView.findViewById(R.id.Runtime);
+        TextView    rating      = (TextView) rootView.findViewById(R.id.Rating);
+        TextView    description = (TextView) rootView.findViewById(R.id.Description);
+
+        title.setText(oneMovieData.get(TITLE_TAG).toString());
+        Picasso.with(getContext())
+                .load(THUMBNAIL_TAG)
+                //.resize(150, 150)
+                .into(thumbnail);
+        releaseDate.setText(oneMovieData.get(RELEASE_DATE_TAG).toString());
+        runtime.setText("who knows");
+        rating.setText(oneMovieData.get(RATING_TAG).toString());
+        description.setText(oneMovieData.get(OVERVIEW_TAG).toString());
 
 
         return rootView;
